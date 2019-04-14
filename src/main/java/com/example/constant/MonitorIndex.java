71184beae72395ec1a928fc16947b50635302a7c
DexianChen <dex2015@aliyun.com>
2019-04-14 10:15:50 +0800

    feat():添加actuator的demo

---
+++ src/main/java/com/example/constant/MonitorIndex.java
@@ -0,0 +1,12 @@
+package com.example.constant;
+
+public interface MonitorIndex {
+    /**
+     * test
+     */
+    String TEST = "test";
+
+    interface Labels{
+        String KEY = "KEY";
+    }
+}
